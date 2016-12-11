2fdd5322b35fcd58cb6cd8c2e1eab243d6200708
jinpu <jinpu@LAPTOP-J1SUAON8>
2016-12-11 22:53:59 +0900

    テストケース修正

--- src/test/java/net/will_co21/format/json/JsonStringSerializableTest.java
+++ src/test/java/net/will_co21/format/json/JsonStringSerializableTest.java
@@ -44,22 +44,22 @@ public class JsonStringSerializableTest {
 				"\uDB40\uDC00\uDB40\uDC7F\uDB3F\uDC00\uDB40\uDBFF\uDB40\uDC80" +
 				"\uD800\uDC00\uD800\uDFFF\uDBFF\uDC00\uDBFF\uDFFF" +
 				"\uD7FF\uDC00\uD800\uD800\uE000\uDBFF\uDBFF\uDBFF\uE000" +
-				"\u0080\uD7FF\u007F\uE000\uFFFF\uDFFF\u0100\b\f\n\r\t<>&'/\"\u001F\u007F";
+				"\u0080\uD7FF\u007F\uD800\uE000\uFFFF\uDFFF\u0100\b\f\n\r\t<>&'/\"\u001F\u007F";
 
 		String json = (new JsonStringSerializable(str, new JsonOptions(new JsonOption[] {}))).toJson();
 
-		assertEquals("\\u0080\\u009F\\u007F\u00A0" +
+		assertEquals("\"\\u0080\\u009F\\u007F\u00A0" +
 				"\\u200B\\u200F\u200A\u2010" +
 				"\\u2028\\u202F\u2027\u2030" +
 				"\\u2060\\u2064\u205F\u2065" +
 				"\\u2066\\u206F\u2070" +
-				"\\u200E\\u200F\u200D\u2010" +
-				"\\u202A\\u202E\u2029\u202F" +
+				"\\u200E\\u200F\\u200D\u2010" +
+				"\\u202A\\u202E\\u2029\\u202F" +
 				"\\uFFF0\\uFFFF\uFFEF" +
 				"\\uDB40\\uDC00\\uDB40\\uDC7F\uDB3F\uDC00\\uDB40\\uDBFF\uDB40\uDC80" +
 				"\uD800\uDC00\uD800\uDFFF\uDBFF\uDC00\uDBFF\uDFFF" +
-				"\uD7FF\\uDC00\\uD800\\uD800\\uE000\\uDBFF\\uDBFF\\uDBFF\\uE000" +
-				"\u0080\uD7FF\\u007F\uE000\\uFFFF\uDFFF\\u0100\\b\\f\\n\\r\\t<>&'/\\\"\\u001F\\u007F", json);
+				"\uD7FF\\uDC00\\uD800\\uD800\uE000\\uDBFF\\uDBFF\\uDBFF\\uE000" +
+				"\\u0080\uD7FF\\u007F\\uD800\\uE000\\uFFFF\\uDFFF\u0100\\b\\f\\n\\r\\t<>&'\\/\\\"\\u001F\\u007F\"", json);
 	}
 
 	@Test
